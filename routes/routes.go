3297144bd97e4cd39925d232ea719cee76166376
Luizfpereira <luizpereira.mec@outlook.com>
2021-10-15 17:29:37 -0300

    updating route handlers

---
+++ routes/routes.go
@@ -0,0 +1,11 @@
+// routes.go
+
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func InitializeRoutes(router *gin.Engine) {
+
+	// Handle the index route
+	//router.GET("/", ShowIndexPage)
+}
