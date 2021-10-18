af843356d7976c22df0095660255cd7b845fd28a
Luizfpereira <luizpereira.mec@outlook.com>
2021-10-18 16:04:08 -0300

    end of app

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// main.go
+
 package main
 
 import (
@@ -9,26 +11,29 @@ import (
 var router *gin.Engine
 
 func main() {
+	// Set Gin to production mode
+	gin.SetMode(gin.ReleaseMode)
+
 	// Set the router as the default one provided by Gin
-	router := gin.Default()
+	router = gin.Default()
+
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
 
 	// Initialize the routes
-	//routes.InitializeRoutes(router)
-	router.GET("/", showIndexPage)
-	router.GET("/article/view/:article_id", getArticle)
+	initializeRoutes()
 
 	// Start serving the application
-	router.Run(":8000")
-
+	router.Run()
 }
 
 // Render one of HTML, JSON or CSV based on the 'Accept' header of the request
 // If the header doesn't specify this, HTML is rendered, provided that
 // the template name is present
 func render(c *gin.Context, data gin.H, templateName string) {
+	loggedInInterface, _ := c.Get("is_logged_in")
+	data["is_logged_in"] = loggedInInterface.(bool)
 
 	switch c.Request.Header.Get("Accept") {
 	case "application/json":
@@ -41,5 +46,4 @@ func render(c *gin.Context, data gin.H, templateName string) {
 		// Respond with HTML
 		c.HTML(http.StatusOK, templateName, data)
 	}
-
 }
